592ffcfe41d965a052c8eb641905df796bc901e7
Ermek Barmashev <barmashev48@gmail.com>
2020-11-13 10:49:22 -0500

    changes

--- script.js
+++ script.js
@@ -20,6 +20,31 @@ myApp.cards = {
     ranks:[2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
 }
 
+myApp.startGame = function(){
+    $('.startGame').on('click', myApp.init);
+}  
+
+myApp.init = function(){
+    myApp.$player.empty();
+    myApp.$dealer.empty();
+
+    myApp.hand.player = [0];
+    myApp.hand.dealer = [0];
+    
+    myApp.results.player = '';
+    myApp.results.dealer = '';
+    $('.modal').slideUp('slow');
+    myApp.clickHit();
+    myApp.clickStand();
+
+    
+        myApp.dealCards(myApp.$player);
+        // myApp.dealCards(myApp.$dealer);
+        // myApp.dealCards(myApp.$player);
+        // myApp.dealCards(myApp.$dealer);
+    
+}
+
 myApp.dealCards = function(side){
     let rank = myApp.getRandomSign('ranks');
     let suit = myApp.getRandomSign('suits');
@@ -40,14 +65,14 @@ myApp.dealCards = function(side){
     }
 }
 
-myApp.getRandomNumber = function(number){
-    return Math.floor(Math.random() * number);
-}
-
 myApp.getRandomSign = function(sign) {
     const randomIndex = myApp.getRandomNumber(myApp.cards[sign].length)
     const randomSign = myApp.cards[sign][randomIndex];
     return randomSign;
+}
+
+myApp.getRandomNumber = function(number){
+    return Math.floor(Math.random() * number);
 }
 
 myApp.setCardScore = function(side, rank ){
@@ -75,64 +100,34 @@ myApp.over21 = function(score){
     }
 }
 
-myApp.standHandler = function(){
-    const player = myApp.results.player;
-    const dealer = myApp.results.dealer;
+// myApp.standHandler = function(){
+//     const player = myApp.results.player;
+//     const dealer = myApp.results.dealer;
 
-    myApp.over21('dealer', dealer);
+//     myApp.over21('dealer', dealer);
 
-    if(dealer > player && dealer <= 21) {
-        myApp.gameOver('lost');
-    } 
-    else if(dealer > 21){
-        myApp.gameOver('won');
-    }
-    else {
-        myApp.dealCards(myApp.$dealer);
-        console.log('dealer drawing');
-        myApp.standHandler();
-    }
-}
-
-myApp.init = function(){
-    myApp.$player.empty();
-    myApp.$dealer.empty();
-
-    myApp.hand.player = [0];
-    myApp.hand.dealer = [0];
-    
-    myApp.results.player = '';
-    myApp.results.dealer = '';
-    $('.modal').slideUp('slow');
-    myApp.clickHit();
-    myApp.clickStand();
-
-    for(let i = 0; i < 2; i++) {
-        myApp.dealCards(myApp.$player);
-        myApp.dealCards(myApp.$dealer);
-    }
-}
-
-myApp.startGame = function(){
-    $('.startGame').on('click', myApp.init);
-}  
+//     if(dealer > player && dealer <= 21) {
+//         myApp.gameOver('lost');
+//     } 
+//     else if(dealer > 21){
+//         myApp.gameOver('won');
+//     }
+//     else {
+//         myApp.dealCards(myApp.$dealer);
+//         console.log('dealer drawing');
+//         myApp.standHandler();
+//     }
+// }
 
 myApp.gameOver = function(verb){
-    setTimeout(() => {
-        myApp.$resultsText.text(`You have ${verb}`);
-        
+    myApp.$resultsText.text(`You have ${verb}`);
+    
+    $('.modal').slideDown('slow');
 
-        $('.modal').slideDown('slow');
-
-        myApp.startGame();
-
-    }, 1000);
+    myApp.startGame();
 }
 
-
 //Event listeners
-
-
 myApp.clickHit = function(){
     $('.hit').on('click', function(){
     myApp.dealCards(myApp.$player);
@@ -143,10 +138,16 @@ myApp.clickStand = function(){
     $('.stand').on('click', myApp.standHandler);
 }    
 
-
 $(function(){
     myApp.startGame(); 
 })
+
+
+
+
+
+
+
 
 
 
